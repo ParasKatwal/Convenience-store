a61b49a91832955e7c21abd4ac3b719a8288d5c9
ParasKatwal <katwal.paras@gmail.com>
2020-09-04 23:59:51 +0545

    updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,71 +14,71 @@ import SingleProduct from "../components/SingleProduct";
 import FeaturedProducts from "../pages/FeaturedProducts";
 
 function Home(props) {
-  return (
-    <>
-      <div className="hero">
-        <ProductList />
-        <Container>
-          <div className="hero__bg">
-            <img src={heroBg} alt="" />
-          </div>
-          <div className="hero__card">
-            <div className="card">
-              <Row>
-                <Col sm={12} md={6} lg={3}>
-                  <div className="card__item">
-                    <img src={cheap} alt="" />
-                    <div className="pl-3">
-                      <p>Cheap Shipping</p>
-                      <span>Very low price</span>
+    return (
+        <>
+            <div className="hero">
+                <Container>
+                    <div className="hero__bg">
+                        <img src={heroBg} alt="" />
                     </div>
-                  </div>
-                </Col>
-                <Col sm={12} md={6} lg={3}>
-                  <div className="card__item">
-                    <img src={money} alt="" />
-                    <div className="pl-3">
-                      <p>100% Money Back</p>
-                      <span>30 days money back</span>
+                    <div className="hero__card">
+                        <div className="card">
+                            <Row>
+                                <Col sm={12} md={6} lg={3}>
+                                    <div className="card__item">
+                                        <img src={cheap} alt="" />
+                                        <div className="pl-3">
+                                            <p>Cheap Shipping</p>
+                                            <span>Very low price</span>
+                                        </div>
+                                    </div>
+                                </Col>
+                                <Col sm={12} md={6} lg={3}>
+                                    <div className="card__item">
+                                        <img src={money} alt="" />
+                                        <div className="pl-3">
+                                            <p>100% Money Back</p>
+                                            <span>30 days money back</span>
+                                        </div>
+                                    </div>
+                                </Col>
+                                <Col sm={12} md={6} lg={3}>
+                                    <div className="card__item">
+                                        <img src={hour} alt="" />
+                                        <div className="pl-3">
+                                            <p>Help Center</p>
+                                            <span>24/7 Support System</span>
+                                        </div>
+                                    </div>
+                                </Col>
+                                <Col sm={12} md={6} lg={3}>
+                                    <div className="card__item">
+                                        <img src={payment} alt="" />
+                                        <div className="pl-3">
+                                            <p>Payment Method</p>
+                                            <span>Secure payment</span>
+                                        </div>
+                                    </div>
+                                </Col>
+                            </Row>
+                        </div>
                     </div>
-                  </div>
-                </Col>
-                <Col sm={12} md={6} lg={3}>
-                  <div className="card__item">
-                    <img src={hour} alt="" />
-                    <div className="pl-3">
-                      <p>Help Center</p>
-                      <span>24/7 Support System</span>
-                    </div>
-                  </div>
-                </Col>
-                <Col sm={12} md={6} lg={3}>
-                  <div className="card__item">
-                    <img src={payment} alt="" />
-                    <div className="pl-3">
-                      <p>Payment Method</p>
-                      <span>Secure payment</span>
-                    </div>
-                  </div>
-                </Col>
-              </Row>
+                </Container>
+                <ProductList />
             </div>
-          </div>
-        </Container>
-      </div>
-      {props.userId && (
-        <div className="feature">
-          <FeaturedProducts />
-        </div>
-      )}
-    </>
-  );
+            {props.userId && (
+                <div className="feature">
+                    <FeaturedProducts />
+                </div>
+            )}
+        </>
+    );
 }
 
 const mapStateToProps = (state) => {
-  return {
-    userId: state.userData._id,
-  };
+    return {
+        userId: state.userData._id,
+    };
 };
 
 export default connect(mapStateToProps)(Home);
